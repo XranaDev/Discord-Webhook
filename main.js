fe5b8ae573ce4437131c80122bfb2bc68f8b151e
XranaDev <79064535+XranaDev@users.noreply.github.com>
2022-04-19 10:19:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(function(){
     $('#btn').click(function(){
         var link = $('https://webhook.site/ca91bc8e-ff80-49a4-922c-64feec9bb474?').val();
-        var username = $('#username').val();
+        var username = $('#name').val();
         var content = $('#content').val();
         if (content==null || content=="")
         {
             alert("Please Fill Out All The Fields");
             return false;
         }
-        $.post(link, {"content": "> **USERNAME  : **" + document.getElementById('username').value + "\n> **MESSAGE  : **" + document.getElementById('content').value);
+        $.post(link, {"content": "> **USERNAME  : **" + document.getElementById('name').value + "\n> **MESSAGE  : **" + document.getElementById('content').value);
     });
 });
